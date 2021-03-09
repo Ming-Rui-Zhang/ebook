be7e0b218c514e530187fb520c02923cb14e8783
Ming-Rui-Zhang <1299722697@qq.com>
2021-03-09 10:52:35 +0800

    1改

--- src/views/store/StoreDetail.vue
+++ src/views/store/StoreDetail.vue
@@ -381,8 +381,6 @@
               color: #333;
             }
           }
-          #preview {
-          }
           .book-detail-content-item-wrapper {
             .book-detail-content-item {
               padding: px2rem(15) 0;
